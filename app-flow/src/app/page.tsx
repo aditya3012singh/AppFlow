5c41c4b003ca78ef7d99e5a8512ec168359f8ddc
gitPushAditya <adityaanandofficial30102000@gmail.com>
2025-07-02 22:05:03 +0530

    Added App Flow

---
+++ app-flow/src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to App Flow</h1>
+      <p>
+        Huhhh...this is our base page. But this in not where app will start. This will have a button for Launch App.
+      </p>
+    </main>
+  );
+}
